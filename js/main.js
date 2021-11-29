d9e2b2a1a39178d2fdfa723c1491a0e46ad8c354
Hall0p <nazarov_rostislav@mail.ru>
2021-11-29 21:58:27 +0300

    create: sticky elements

--- js/main.js
+++
@@ -1,7 +0,0 @@
-var transformIntoNiger = document.querySelector(".menu-button");
-transformIntoNiger.addEventListener("click", function () {
-  console.log("клик по кнопке меню");
-  document
-    .querySelector(".navbar-bottom")
-    .classList.toggle("navbar-bottom--visible");
-});
