922f7e118b537e396ad83b5bb53fe1b9af550441
Hall0p <nazarov_rostislav@mail.ru>
2021-12-02 03:35:32 +0300

    create: services

---
+++ smth.php
@@ -0,0 +1,11 @@
+<?php include('header.php')?>
+<div class="container">
+  <h2 class="hero__title"></h2>
+  <div class="wrapper">
+
+<?php include('sidebar.php')?>
+</div>
+  <!-- /.wrapper -->
+</div>
+<!-- /.container -->
+<?php include('footer.php')?>
